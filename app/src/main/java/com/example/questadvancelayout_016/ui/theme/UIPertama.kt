d5ebe58324130bc3891c5b0c3a5b9f6f69fcae6c
HafizZada16 <hafizzada16@gmail.com>
2025-10-24 15:00:40 +0700

    Membuat AktivitasPertama

---
+++ app/src/main/java/com/example/questadvancelayout_016/ui/theme/UIPertama.kt
@@ -0,0 +1,34 @@
+package com.example.questadvancelayout_016.ui.theme
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.questadvancelayout_016.R
+
+
+@Composable
+fun AktivitasPertama(modifier: Modifier){
+    Column(modifier= Modifier
+        .padding(top = 100.dp)
+        .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        Text(
+            text = stringResource(id= R.string.prodi),
+            fontSize = 30.sp,
+            fontWeight = FontWeight.Bold
+        )
+
+        Text(
+            text = stringResource(id= R.string.univ),
+            FontSize = 10.sp
+        )
+    }}
