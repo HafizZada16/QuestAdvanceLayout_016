177bf31bc09b1a4d72f0ecc95b082ca0a839f8d4
HafizZada16 <hafizzada16@gmail.com>
2025-10-25 01:10:34 +0700

    Membuat Row untuk Icon Medsos

--- app/src/main/java/com/example/questadvancelayout_016/ui/theme/UITugas.kt
+++ app/src/main/java/com/example/questadvancelayout_016/ui/theme/UITugas.kt
@@ -3,7 +3,9 @@ package com.example.questadvancelayout_016.ui.theme
 import android.R
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
@@ -15,6 +17,7 @@ import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
@@ -69,6 +72,11 @@ fun ProfileScreen() {
                 fontSize = 16.sp
             )
             Spacer(modifier = Modifier.height(12.dp))
+
+            Row(
+                horizontalArrangement = Arrangement.Center,
+                verticalAlignment = Alignment.CenterVertically
+            ){}
         }
     }
 }
