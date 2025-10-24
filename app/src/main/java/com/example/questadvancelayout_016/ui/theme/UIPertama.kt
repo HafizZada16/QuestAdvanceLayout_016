beed5805b4ce5538061e3e98c91960e8c4d73bca
HafizZada16 <hafizzada16@gmail.com>
2025-10-24 15:36:55 +0700

    Menambahkan Spacer dan Columns name

--- app/src/main/java/com/example/questadvancelayout_016/ui/theme/UIPertama.kt
+++ app/src/main/java/com/example/questadvancelayout_016/ui/theme/UIPertama.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
@@ -20,6 +21,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ModifierLocalBeyondBoundsLayout
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -65,9 +67,19 @@ fun AktivitasPertama(modifier: Modifier){
                     contentDescription = null,
                     modifier = Modifier
                         .size(size = 100.dp)
-                        .padding(all = 5.dp)
+                        .padding(5.dp)
                 )
 
+                Spacer(modifier = Modifier.width(30.dp))
+                Column(){
+                    Text(
+                        stringResource(R.string.nama),
+                        fontSize = 30.sp,
+                        fontFamily = FontFamily.Cursive,
+                        color = Color.White,
+                        modifier = Modifier.padding(top = 15.dp)
+                    )
+                }
             }
         }
 
