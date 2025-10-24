779fe6d6675bcdc97495aa593de2ab3466ce949c
HafizZada16 <hafizzada16@gmail.com>
2025-10-25 01:01:38 +0700

    Membuat ui Image Foto Profil

--- app/src/main/java/com/example/questadvancelayout_016/ui/theme/UITugas.kt
+++ app/src/main/java/com/example/questadvancelayout_016/ui/theme/UITugas.kt
@@ -1,17 +1,26 @@
 package com.example.questadvancelayout_016.ui.theme
 
 import android.R
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import com.example.questadvancelayout_016.R
 
 @Composable
 fun ProfileScreen() {
@@ -26,6 +35,17 @@ fun ProfileScreen() {
                 .padding(horizontal = 24.dp)
         ){
             Spacer(modifier = Modifier.height(56.dp))
+
+            Image(
+                painter = painterResource(R.drawable.pp),
+                contentDescription = "Foto Profil",
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .size(120.dp)
+                    .clip(CircleShape)
+                    .border(2.dp, Color.White, CircleShape)
+            )
+            Spacer(modifier = Modifier.height(16.dp))
         }
     }
 }
