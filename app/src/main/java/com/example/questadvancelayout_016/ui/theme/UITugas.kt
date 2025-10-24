b49a4ae38878dff358347c91d1da1597cb9fd3cf
HafizZada16 <hafizzada16@gmail.com>
2025-10-25 00:40:08 +0700

    Membuat fungsi ProfileScreen dan Surface nya

--- app/src/main/java/com/example/questadvancelayout_016/ui/theme/UITugas.kt
+++ app/src/main/java/com/example/questadvancelayout_016/ui/theme/UITugas.kt
@@ -1,5 +1,20 @@
 package com.example.questadvancelayout_016.ui.theme
 
+import android.R
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 
 @Composable
+fun ProfileScreen() {
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = DarkBackground
+    ) {
+        Column(
+
+        )
+    }
+}
