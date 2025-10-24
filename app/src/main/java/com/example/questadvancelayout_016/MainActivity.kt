1f0ccc7a4ecad2270791d9ee366c202331782700
HafizZada16 <hafizzada16@gmail.com>
2025-10-24 16:08:39 +0700

    Agar dapat di tampilkan

--- app/src/main/java/com/example/questadvancelayout_016/MainActivity.kt
+++ app/src/main/java/com/example/questadvancelayout_016/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.questadvancelayout_016.ui.theme.AktivitasPertama
 import com.example.questadvancelayout_016.ui.theme.QuestAdvanceLayout_016Theme
 
 class MainActivity : ComponentActivity() {
@@ -20,8 +21,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             QuestAdvanceLayout_016Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    AktivitasPertama(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -30,18 +30,3 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    QuestAdvanceLayout_016Theme {
-        Greeting("Android")
-    }
-}
