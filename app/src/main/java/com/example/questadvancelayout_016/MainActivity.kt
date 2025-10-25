bf876308fe6554e708b2ce184ff1dfef3400baf7
HafizZada16 <hafizzada16@gmail.com>
2025-10-25 07:48:49 +0700

    merubah content ke yg praktikum

--- app/src/main/java/com/example/questadvancelayout_016/MainActivity.kt
+++ app/src/main/java/com/example/questadvancelayout_016/MainActivity.kt
@@ -7,11 +7,8 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.questadvancelayout_016.ui.theme.AktivitasPertama
+import com.example.questadvancelayout_016.ui.theme.ProfileScreen
 import com.example.questadvancelayout_016.ui.theme.QuestAdvanceLayout_016Theme
 
 class MainActivity : ComponentActivity() {
@@ -21,7 +18,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             QuestAdvanceLayout_016Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    AktivitasPertama(
+                    ProfileScreen(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
